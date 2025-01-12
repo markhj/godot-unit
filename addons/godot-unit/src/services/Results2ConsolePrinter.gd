ee515cff22a7854ddb89c09fec78ec864c4a0e42
mhj <5261387+markhj@users.noreply.github.com>
2025-01-12 12:54:27 +0100

    Adjusted Results2ConsolePrinter for AssertionMeta object

--- addons/godot-unit/src/services/Results2ConsolePrinter.gd
+++ addons/godot-unit/src/services/Results2ConsolePrinter.gd
@@ -5,14 +5,14 @@ class_name Results2ConsolePrinter
 func print_results(results: TestResult) -> void:
 	print("=".repeat(32) + "\nGodotUnit\n")
 	
-	for err in results.failed:
-		print("[FAILED] %s" % [err.message])
+	for err in results.assertions:
+		print("[FAILED] %s" % [err.it])
 		print(" ".repeat(9) + "Expected: %s" % [str(err.expected)])
 		print(" ".repeat(9) + "Actual  : %s" % [str(err.actual)])
 		print(" ".repeat(9) + "File: %s | Method: %s | Line: %d" % [
-			err.test_case_file,
-			err.test_case_method,
-			err.test_case_line,
+			err.source.file,
+			err.source.function,
+			err.source.line,
 		])
 		print("")
 	
