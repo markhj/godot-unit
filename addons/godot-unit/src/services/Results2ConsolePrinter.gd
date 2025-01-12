26e4fce00f70c4de8a6ae8cea1a2ac2c23edf837
mhj <5261387+markhj@users.noreply.github.com>
2025-01-12 13:31:47 +0100

    Only print failed cases to console

--- addons/godot-unit/src/services/Results2ConsolePrinter.gd
+++ addons/godot-unit/src/services/Results2ConsolePrinter.gd
@@ -5,16 +5,17 @@ class_name Results2ConsolePrinter
 func print_results(results: TestResult) -> void:
 	print("=".repeat(32) + "\nGodotUnit\n")
 	
-	for err in results.assertions:
-		print("[FAILED] %s" % [err.it])
-		print(" ".repeat(9) + "Expected: %s" % [str(err.expected)])
-		print(" ".repeat(9) + "Actual  : %s" % [str(err.actual)])
-		print(" ".repeat(9) + "File: %s | Method: %s | Line: %d" % [
-			err.source.file,
-			err.source.function,
-			err.source.line,
-		])
-		print("")
+	for assertion in results.assertions:
+		if not assertion.passed:
+			print("[FAILED] %s" % [assertion.it])
+			print(" ".repeat(9) + "Expected: %s" % [str(assertion.expected)])
+			print(" ".repeat(9) + "Actual  : %s" % [str(assertion.actual)])
+			print(" ".repeat(9) + "File: %s | Method: %s | Line: %d" % [
+				assertion.source.file,
+				assertion.source.function,
+				assertion.source.line,
+			])
+			print("")
 	
 	print("TOTAL: %d | PASSED: %d" % [
 		results.tests,
